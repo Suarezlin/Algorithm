0170a461f3dc1449582681a1778a930e6db80e24
Suarez Lin <1031312670@qq.com>
2017-11-28 23:13:44 +0800

    第一章 1.1.1 至 1.1.14

---
+++ src/第1章_基础/demo.java
@@ -0,0 +1,9 @@
+package 第1章_基础;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class demo {
+    public static void main(String[] args) {
+        StdOut.printf("Name: %s", "Suarezlin");
+    }
+}
---
+++ src/第1章_基础/习题/Question_1_1_8.java
@@ -0,0 +1,10 @@
+public class Question_1_1_8 {
+    public static void main(String[] args) {
+        // 输出字符 'b'
+        System.out.println('b');
+        // 字符 'b' 与 'c' 相加，结果为整数，为 b c ASCII 码值之和
+        System.out.println('b' + 'c');
+        // 'a' + 4 结果转换为字符为 a 之后第四个字符：e
+        System.out.println((char)('a' + 4));
+    }
+}
