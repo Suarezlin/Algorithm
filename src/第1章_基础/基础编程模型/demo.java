49e253812245779ee1cc1c345bac50f8a6922e7c
Suarez Lin <1031312670@qq.com>
2017-11-29 17:03:45 +0800

    .gitignore update

--- src/第1章_基础/基础编程模型/demo.java
+++
@@ -1,9 +0,0 @@
-package 第1章_基础;
-
-import edu.princeton.cs.algs4.StdOut;
-
-public class demo {
-    public static void main(String[] args) {
-        StdOut.printf("Name: %s", "Suarezlin");
-    }
-}
