b5a20cc65eecf2d5993baad8bc137d008749da8c
Suarez Lin <1031312670@qq.com>
2017-12-01 21:52:13 +0800

    精简 1-2-10 代码

--- src/第1章_基础/习题/数据抽象/Question_1_2_10.java
+++ src/第1章_基础/习题/数据抽象/Question_1_2_10.java
@@ -1,6 +1,8 @@
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.lang.reflect.Array;
+
 public class Question_1_2_10 {
     private int N;
     private int max;
@@ -14,54 +16,40 @@ public class Question_1_2_10 {
         this.count = 0;
     }
 
+    private int[] getAxis(int i) {
+        if ((i % 5) != 0 && (i / 25) != 1) {
+            int x = 2 * (i % 5) - 1;
+            int y = 2 * ((i - 1) / 5 + 1) - 1;
+            int[] result = {x, y};
+            return result;
+        } else if ((i % 5) == 0 && (i / 25) != 1) {
+            int x = 9;
+            int y = 2 * ((i - 1) / 5 + 1) - 1;
+            int[] result = {x, y};
+            return result;
+        } else if ((i % 5) != 0 && (i / 25) == 1) {
+            int x = 2 * (i % 5) - 1;
+            int y = 9;
+            int[] result = {x, y};
+            return result;
+        } else {
+            int x = 9;
+            int y = 9;
+            int[] result = {x, y};
+            return result;
+        }
+    }
+
     private void draw() {
         StdDraw.clear();
         StdDraw.setXscale(0, 10);
         StdDraw.setYscale(0, 10);
         StdDraw.setPenRadius(0.01);
         for (int i = 1; i <= max; i++) {
-            if ((i % 5) != 0 && (i / 25) != 1) {
-                int x = 2 * (i % 5) - 1;
-                int y = 2 * ((i - 1) / 5 + 1) - 1;
-                StdDraw.rectangle(x, y, 0.5, 0.5);
-            }
-            else if ((i % 5) == 0 && (i / 25) != 1) {
-                int x = 9;
-                int y = 2 * ((i - 1) / 5 + 1) - 1;
-                StdDraw.rectangle(x, y, 0.5, 0.5);
-            }
-            else if ((i % 5) != 0 && (i / 25) == 1) {
-                int x = 2 * (i % 5) - 1;
-                int y = 9;
-                StdDraw.rectangle(x, y, 0.5, 0.5);
-            }
-            else {
-                int x = 9;
-                int y = 9;
-                StdDraw.rectangle(x, y, 0.5, 0.5);
-            }
-        }
-        for (int i = 1; i <= count; i++) {
-            if ((i % 5) != 0 && (i / 25) != 1) {
-                int x = 2 * (i % 5) - 1;
-                int y = 2 * ((i - 1) / 5 + 1) - 1;
-                StdDraw.filledRectangle(x, y, 0.5, 0.5);
-            }
-            else if ((i % 5) == 0 && (i / 25) != 1) {
-                int x = 9;
-                int y = 2 * ((i - 1) / 5 + 1) - 1;
-                StdDraw.filledRectangle(x, y, 0.5, 0.5);
-            }
-            else if ((i % 5) != 0 && (i / 25) == 1) {
-                int x = 2 * (i % 5) - 1;
-                int y = 9;
-                StdDraw.filledRectangle(x, y, 0.5, 0.5);
-            }
-            else {
-                int x = 9;
-                int y = 9;
-                StdDraw.filledRectangle(x, y, 0.5, 0.5);
-            }
+            int x = getAxis(i)[0];
+            int y = getAxis(i)[1];
+            StdDraw.rectangle(x, y, 0.5, 0.5);
+            if (i <= count) StdDraw.filledRectangle(x, y, 0.5, 0.5);
         }
         StdDraw.show(500);
     }
